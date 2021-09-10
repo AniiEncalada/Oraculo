5792574eaa1fd9a11a6e6da3f9406b3e4eb42558
Anii <maria.encalada1980@gmail.com>
2021-09-10 09:42:03 -0500

    Reset

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import { Main } from "./app/pages/main";
-
-function App() {
-  return <Main />;
-}
-
-export default App;
--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import oraculoReducer from "../features/oraculo/oraculoSlice";
-
-export const store = configureStore({
-  reducer: {
-    oraculo: oraculoReducer,
-  },
-});
